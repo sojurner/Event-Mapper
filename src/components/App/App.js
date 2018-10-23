0354237b56b893724a4e3c68eaa94ac858d97503
sojurner <pykim0591@gmail.com>
2018-10-23 16:14:07 -0600

    Create google components for login and logout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
-
 export class App extends Component {
   responseGoogle = res => {
     console.log(res);
@@ -16,10 +15,17 @@ export class App extends Component {
   render() {
     return (
       <div className="App">
+        <button>
+          <GoogleLogin
+            clientId="241779944002-qa42v6cvk3kpu8da0jimdr364vn41t1r.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+          />
+          <GoogleLogout buttonText="Logout" onLogoutSuccess={this.logout} />
+        </button>
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p />
           <a
             className="App-link"
             href="https://reactjs.org"
