a0552614fa4bb0970cab7e995d0d3aba07a7ad90
Joel O'Connor <joconno07@gmail.com>
2018-10-23 22:46:13 -0600

    Built out getEvents fetch to ticket master

---
+++ src/utilities/apiCalls/apiCalls.js
@@ -0,0 +1,11 @@
+import { ticketMasterApiKey } from './apiKeys';
+import { eventsCleaner } from '../helpers/helpers';
+
+export const getEvents = async (location, radius) => {
+  const url = 'https://app.ticketmaster.com/discovery/v2/events.json?'
+    +`apikey=${ticketMasterApiKey}&`
+    + 'geoPoint=9xj64fk3s';
+  const response = await fetch(url);
+  const cleanedEvents = eventsCleaner(await response.json());
+  return cleanedEvents;
+};
