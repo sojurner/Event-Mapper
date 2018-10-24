1232502613ec01ec09d37dd9aadd1e9f958a52ed
Joel O'Connor <joconno07@gmail.com>
2018-10-23 23:50:30 -0600

    map files

---
+++ src/components/Map/Map.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import { mbAccessToken as Token } from '../../data/apiKeys';
+
+class Map extends Component {
+  constructor() {
+    super();
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    };
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  async componentDidMount() {
+    await navigator.geolocation.getCurrentPosition(async location => {
+      await this.renderMap(location);
+    });
+  }
+
+  renderMap = location => {
+    this.setState({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    });
+  };
+
+  render() {
+    const Map = ReactMapboxGl({
+      accessToken: Token
+    });
+    const { latitude, longitude } = this.state;
+    return (
+      <Map
+        center={[longitude, latitude]}
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw'
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[0, 0]} />
+        </Layer>
+      </Map>
+    );
+  }
+}
+
+export default Map;
