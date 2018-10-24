9643869a79ef0b32dfb1fbfb9830cb6c26730d55
Joel O'Connor <joconno07@gmail.com>
2018-10-24 14:20:35 -0600

    Built postUser fetch call

--- src/utilities/apiCalls/apiCalls.js
+++ src/utilities/apiCalls/apiCalls.js
@@ -1,9 +1,22 @@
 import { ticketMasterApiKey } from './apiKeys';
-import { eventsCleaner } from '../helpers/helpers';
+import { eventsCleaner, cleanedUser } from '../helpers/helpers';
 
 export const getEvents = async (location, radius) => {
   const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketMasterApiKey}&geoPoint=9xj64fk3s`;
   const response = await fetch(url);
   const cleanedEvents = eventsCleaner(await response.json());
   return cleanedEvents;
+};
+
+export const postUser = async (userInfo) => {
+  const activeUser = await cleanedUser(userInfo);
+  console.log(activeUser);
+  
+  const url = 'https://event-mapper-api.herokuapp.com/api/v1/users';
+  const response = await fetch(url, {
+    method: 'POST',
+    body: activeUser,
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const result = await response.json();
 };
