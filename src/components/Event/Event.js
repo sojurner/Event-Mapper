cbed24c8d70cc204bcaa99934c55b99bcea8e2cd
Joel O'Connor <joconno07@gmail.com>
2018-10-24 19:25:29 -0600

    Removed unneeded code and log, linted file

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import { Marker } from 'react-mapbox-gl';
 import './Event.css';
-
-import { Layer, Feature, Marker } from 'react-mapbox-gl';
 
 export const Event = ({ events }) => {
   return events.map((event, index) => {
     let coordinates = [event.venues[0].lng, event.venues[0].lat];
-    console.log(coordinates);
     return (
       <Marker
         key={`event-${index}`}
