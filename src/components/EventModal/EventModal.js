9cdf9b6f983c502bd8777ffefc0159165608917d
sojurner <pykim0591@gmail.com>
2018-10-25 06:38:04 -0600

    Adjust object desctructuring

--- src/components/EventModal/EventModal.js
+++ src/components/EventModal/EventModal.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-export const EventModal = ({ targetEvent, favoriteClick }) => {
-  const { name, img, date, venues } = targetEvent;
+export const EventModal = ({ targetEvent, handleFavoriteClick }) => {
+  const { name, img, date, venue_name, favorite, address } = targetEvent;
+  console.log(targetEvent);
   return (
     <div>
-      <i className="fas fa-star" onClick={favoriteClick} />
+      <i
+        className={favorite ? 'fas fa-star' : 'far fa-star'}
+        onClick={handleFavoriteClick}
+      />
       <h1>Event: {name}</h1>
       <h4>Date: {date}</h4>
       <img src={`${img}`} height={400} />
-      <p>Location: {venues[0].name}</p>
-      <p>Address: {venues[0].address}</p>
+      <p>Location: {venue_name}</p>
+      <p>Address: {address}</p>
     </div>
   );
 };
