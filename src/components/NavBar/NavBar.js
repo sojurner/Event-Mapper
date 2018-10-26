3cea48e3b648fedad56e92b208df1747d735e00f
sojurner <pykim0591@gmail.com>
2018-10-26 07:12:32 -0600

    Style navigation container and nav-links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,67 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './NavBar.css';
 
 export class NavBar extends Component {
   render() {
     const { gid, first_name } = this.props.activeUser;
-    const loggedIn = () => {
-      return (
-        <div className='nav-container'>
-          <h1>{`Hello, ${first_name}`}</h1>
-          <div className='button-container'>
-            <Link to={`/${gid}/profile`}>
-              <button className='main-button'>Your Profile</button>
-            </Link>
-            <Link to={`/${gid}/favorites`}>
-              <button className='main-button'>Favorites</button>
-            </Link>
-          </div>
-        </div>
-      );
-    };
-
-    const favorites = () => {
-      return (
-        <div className='nav-container'>
-          <div className='button-container'>
-            <Link to={`/${gid}/profile`}>
-              <button className='main-button'>Your Profile</button>
-            </Link>
-            <Link to={`/${gid}`}>
-              <button className='main-button'>Current Events</button>
-            </Link>
-          </div>
-        </div>
-      );
-    };
-
-    const profile = () => {
-      return (
-        <div className='nav-container'>
-          <Link to={`/${gid}/favorites`}>
-            <button className='main-button'>Favorites</button>
-          </Link>
-          <Link to={`/${gid}`}>
-            <button className='main-button'>Current Events</button>
-          </Link>
-        </div>
-      );
-    };
-
     return (
-      <div className='main-nav'>
-        <Route exact path={`/`} component={loggedIn}></Route>
-        <Route exact path={`/${gid}/favorites`} component={favorites}></Route>
-        <Route exact path={`/${gid}/profile`} component={profile}></Route>
-      </div>
+      <nav className="nav-container">
+        <h1 className="greeting">{`Hello, ${first_name}`}</h1>
+        <NavLink className="nav-link" exact to={`/`}>
+          Home
+        </NavLink>
+        <NavLink className="nav-link" to={`/profile`}>
+          Profile
+        </NavLink>
+        <NavLink className="nav-link" to={`/favorites`}>
+          Favorites
+        </NavLink>
+      </nav>
     );
   }
 }
 
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   activeUser: state.activeUser
 });
 
