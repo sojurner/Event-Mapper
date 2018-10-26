e1226454ff08ccd71a663b1c9c97820914424502
Joel O'Connor <joconno07@gmail.com>
2018-10-25 20:04:04 -0600

    Added alt tag to image

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -32,6 +32,7 @@ class Events extends Component {
         >
           <img
             className="event-marker"
+            alt='map marker for an event'
             src="http://landon-homes.net/wp-content/uploads/2015/04/map-pin.png"
           />
         </Marker>
