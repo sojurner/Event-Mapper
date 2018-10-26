6aa5102a21e0656c204a99c4589d4f385a5174c3
sojurner <pykim0591@gmail.com>
2018-10-26 07:15:52 -0600

    Refactor Routes

---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import App from '../../containers/App/App';
+import Map from '../Map/Map';
+import Profile from '../Profile/Profile';
+import Favorites from '../../containers/Favorites/Favorites';
+
+export const Routes = ({ gid, mapStyle }) => {
+  console.log(gid);
+  return (
+    <div className="main-nav">
+      <Route
+        exact
+        path={`/`}
+        render={() => {
+          return <Map mapStyle={mapStyle} />;
+        }}
+      />
+      <Route exact path={`/${gid}/favorites`} component={Favorites} />
+      <Route exact path={`/${gid}/profile`} component={Profile} />
+    </div>
+  );
+};
