26e492b6297789d0c3a9607c121f9dfdd8c19296
sojurner <pykim0591@gmail.com>
2018-10-26 07:13:25 -0600

    Create dummy Profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>dsdsdsds</div>;
+  }
+}
+
+export default Profile;
