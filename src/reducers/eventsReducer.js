cb37f26e1ee04806fb6ad3660e4a4ab012f1a3d8
sojurner <pykim0591@gmail.com>
2018-10-27 11:50:25 -0600

    Add SET_WATCH_EVENT to reducer

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -2,6 +2,10 @@ export const eventsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_EVENTS':
       return action.events;
+    case 'SET_WATCH_EVENT':
+      const event = state.find(event => event.id === action.event.id);
+      event.favorite = !event.favorite;
+      return [...state, event];
     default:
       return state;
   }
