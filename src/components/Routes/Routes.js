2758a133b8fa6874432def6687bed53a12022625
sojurner <pykim0591@gmail.com>
2018-10-27 09:14:42 -0600

    Add conditional to rendering of Redirect

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import Map from '../Map/Map';
 import Profile from '../Profile/Profile';
-import Favorites from '../../containers/Favorites/Favorites';
+import WatchList from '../../containers/WatchList/WatchList';
+
 import { LoginDisplay } from '../LoginDisplay/LoginDisplay';
 import { HomeDisplay } from '../HomeDisplay/HomeDisplay';
 
 export const Routes = ({
   user,
   mapType,
-  displaySidebar,
-  stateSidebar,
-  changeMap,
-  logout,
+  // displaySidebar,
+  // stateSidebar,
+  // changeMap,
+  // logout,
   login,
   redirect
 }) => {
-  console.log(user);
   return (
     <div className="App">
-      <Route path={`/app/${user}/favorites`} component={Favorites} />
-      <Route path={`/app/${user}/profile`} component={Profile} />
-      {redirect === true && <Redirect to={'/app'} />}
-      {redirect === false && <Redirect to={'/'} />}
+      {!window.location.href.includes('app') &&
+        redirect === true &&
+        user.id && <Redirect to={`/app/${user.id}`} />}
+      {window.location.href.includes('app') &&
+        redirect === false && <Redirect to={`/`} />}
       <Route
         exact
         path={'/'}
