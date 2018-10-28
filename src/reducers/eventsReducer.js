3d12355edef9190fdf0ad56883666d2c4c0e0098
sojurner <pykim0591@gmail.com>
2018-10-28 07:16:02 -0600

    Resolve bug in setWatchEvent case

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -2,10 +2,12 @@ export const eventsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_EVENTS':
       return action.events;
+
     case 'SET_WATCH_EVENT':
       const event = state.find(event => event.e_id === action.event.e_id);
       event.favorite = !event.favorite;
-      return [...state, event];
+      return state;
+
     default:
       return state;
   }
