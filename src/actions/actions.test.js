3f8c9d1691fed5794e4b0c59c714a175b40f0d69
sojurner <pykim0591@gmail.com>
2018-10-27 22:14:35 -0600

    Create Actions tests

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,6 +1,59 @@
-// import React from 'react';
-// import * from './';
+import * as dispatch from './index.js';
+import { mockStateUser } from '../data/mockUser';
+import * as events from '../data/mockEvents';
 
-it('renders without crashing', () => {
-  expect(false).toEqual(false);
+describe('User Actions', () => {
+  it('should return a type of LOGIN_USER', () => {
+    const expected = {
+      type: 'LOGIN_USER',
+      user: mockStateUser
+    };
+    expect(dispatch.loginUser(mockStateUser)).toEqual(expected);
+  });
+});
+
+describe('Watchlist actions', () => {
+  it('should return a type of ADD_TO_WATCH_LIST', () => {
+    const expected = {
+      type: 'ADD_TO_WATCH_LIST',
+      event: events.mockTargetEventTrue
+    };
+
+    expect(dispatch.addToWatchList(events.mockTargetEventTrue)).toEqual(
+      expected
+    );
+  });
+
+  it('should return a type of REMOVE_FROM_WATCH_LIST', () => {
+    const expected = {
+      type: 'REMOVE_FROM_WATCH_LIST',
+      event: events.mockTargetEventFalse
+    };
+
+    expect(dispatch.removeFromWatchlist(events.mockTargetEventFalse)).toEqual(
+      expected
+    );
+  });
+});
+
+describe('Event Actions', () => {
+  it('should return a type pf SET_EVENTS', () => {
+    const expected = {
+      type: 'SET_EVENTS',
+      events: events.eventsResponse
+    };
+
+    expect(dispatch.setEvents(events.eventsResponse)).toEqual(expected);
+  });
+
+  it('should return a type of SET_WATCH_EVENT', () => {
+    const expected = {
+      type: 'SET_WATCH_EVENT',
+      event: events.mockTargetEventFalse
+    };
+
+    expect(dispatch.setWatchEvent(events.mockTargetEventFalse)).toEqual(
+      expected
+    );
+  });
 });
