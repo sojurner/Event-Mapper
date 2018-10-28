e35734fbd11c7b56f47a1ce110a06fcc447c69a0
sojurner <pykim0591@gmail.com>
2018-10-28 15:08:26 -0600

    Create userLocationReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,6 +2,17 @@ export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'LOGIN_USER':
       return action.user;
+
+    default:
+      return state;
+  }
+};
+
+export const userLocationReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_USER_LOCATION':
+      return action.coordinates;
+
     default:
       return state;
   }
