f72df0e2361960a72b42df6f632967e4b6fb1cef
sojurner <pykim0591@gmail.com>
2018-10-28 15:22:55 -0600

    Add apicalls and helpers tests

--- src/utilities/apiCalls/apiCalls.test.js
+++ src/utilities/apiCalls/apiCalls.test.js
@@ -1,4 +1,3 @@
-// import React from 'react';
 import * as call from './apiCalls';
 import * as events from '../../data/mockEvents';
 import * as user from '../../data/mockUser';
@@ -46,4 +45,36 @@ describe('setFavorite', () => {
       params.setFavorite
     );
   });
+
+  describe('removeFromWatchlist', () => {
+    it('should call fetch with correct params', () => {
+      const responseObj = {
+        event: 'G5vzZ4C58cdb-',
+        user: 1
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(responseObj)
+        });
+      });
+
+      call.removeFromWatchlist();
+      expect(window.fetch).toHaveBeenCalledWith(
+        params.removeEvent,
+        params.removeEventOptions
+      );
+    });
+  });
+});
+
+describe('getUserWatchList', () => {
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(events.userWatchlist)
+    });
+  });
+  call.getUserWatchlist();
+  expect(window.fetch).toHaveBeenCalledWith(
+    'https://event-mapper-api.herokuapp.com/api/v1/users/undefined/events'
+  );
 });
