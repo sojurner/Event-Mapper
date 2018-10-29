c7edd1d10830f4d0a5aeb69296f4419f3a27038a
Joel O'Connor <joconno07@gmail.com>
2018-10-29 11:21:31 -0600

    Refactor to have user image in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const Profile = (props) => {
-  const {first_name, last_name, email} = props.activeUser;
+  const {first_name, last_name, email, imageUrl } = props.activeUser;
   return (
-    <div className='user-profile'>
-      <img alt='the current user profile pic' src='https://www.class-central.com/bundles/classcentralsite/images/icon-programming-and-software-development.png'/>
-      <h1>User: {first_name + ' ' + last_name}</h1>
-      <h2>Listed Contact: {email}</h2>
+    <div className='user-wraper'>
+      <div className='user-profile'>
+        <img alt='the current user profile pic' src={imageUrl}/>
+        <h1>User: {first_name + ' ' + last_name}</h1>
+        <h2>Listed Contact: {email}</h2>
+      </div>
     </div>
   );
 };
