9a3779ce44621de2bfb9baf769f2c9fe54c196c1
Joel O'Connor <joconno07@gmail.com>
2018-10-28 22:57:40 -0600

    linted file

--- src/utilities/apiCalls/apiCalls.js
+++ src/utilities/apiCalls/apiCalls.js
@@ -1,17 +1,16 @@
 import { ticketMasterApiKey } from './apiKeys';
 import { eventsCleaner, cleanedUser } from '../helpers/helpers';
-import moment from 'moment';
 import Geohash from 'latlon-geohash';
 
 export const getEvents = async (lat, lng) => {
-  const geoCode = Geohash.encode(lat, lng);
-  let days = 7;
-  let date = new Date();
-  let last = new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
-  const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketMasterApiKey}&geoPoint=${geoCode.slice(
-    0,
-    9
-  )}&radius=50&size=200`;
+  const geoCode = await Geohash.encode(lat, lng);
+  // let days = 7;
+  // let date = new Date();
+  // let last = new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const url = `https://app.ticketmaster.com/discovery/v2/events.json?` +
+    `apikey=${ticketMasterApiKey}&` +
+    `geoPoint=${geoCode.slice(0, 9)}&` +
+    `radius=50&size=200`;
   const response = await fetch(url);
   const result = await response.json();
   const cleanedEvents = eventsCleaner(result);
@@ -47,7 +46,6 @@ export const removeFromWatchlist = async (userId, eventId) => {
     method: 'DELETE',
     headers: { 'Content-Type': 'application/json' }
   });
-
   const result = await response.json();
   return result;
 };
@@ -56,6 +54,5 @@ export const getUserWatchlist = async userId => {
   const url = `https://event-mapper-api.herokuapp.com/api/v1/users/${userId}/events`;
   const response = await fetch(url);
   const result = await response.json();
-  console.log(result);
   return result;
 };
