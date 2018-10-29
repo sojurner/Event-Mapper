e18be6d95b19c2ffd71c1c616e82710678704740
sojurner <pykim0591@gmail.com>
2018-10-29 08:26:23 -0600

    Move remove duplicates to its own function

--- src/utilities/helpers/helpers.js
+++ src/utilities/helpers/helpers.js
@@ -22,17 +22,20 @@ export const eventsCleaner = events => {
       favorite: false
     };
   });
-  function removeDuplicates(arr, prop) {
-    let obj = {};
-    return Object.keys(
-      arr.reduce((prev, next) => {
-        if (!obj[next[prop]]) obj[next[prop]] = next;
-        return obj;
-      }, obj)
-    ).map(i => obj[i]);
-  }
 
-  return removeDuplicates(parsedEvent, 'venue_name');
+  const uniqueEvents = removeDuplicates(parsedEvent, 'venue_name');
+  console.log(uniqueEvents);
+  return uniqueEvents;
+};
+
+const removeDuplicates = (arr, prop) => {
+  let obj = {};
+  return Object.keys(
+    arr.reduce((prev, next) => {
+      if (!obj[next[prop]]) obj[next[prop]] = next;
+      return obj;
+    }, obj)
+  ).map(i => obj[i]);
 };
 
 export const cleanedUser = userInfo => {
