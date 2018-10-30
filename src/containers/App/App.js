a6ffcea6e3df443f32e822f89d31fe6fa463d066
Joel O'Connor <joconno07@gmail.com>
2018-10-30 17:03:36 -0600

    commented out log

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -32,10 +32,10 @@ export class App extends Component {
   };
 
   login = async res => {
-    if (!res.email) {
-      console.log(res);
-      return;
-    }
+    // if (!res.email) {
+    //   console.log(res);
+    //   return;
+    // }
     const activeUser = await postUser(res.profileObj);
     this.props.loginUser(activeUser);
     this.setState({ user: activeUser, redirect: true });
