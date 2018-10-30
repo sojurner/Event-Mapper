567d97801730020625a3d055ffd6f5429382729f
Joel O'Connor <joconno07@gmail.com>
2018-10-30 15:16:40 -0600

    linted file

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Profile.css';
 
-const Profile = (props) => {
-  const {first_name, last_name, email, imageUrl } = props.activeUser;
+export const Profile = props => {
+  const { first_name, last_name, email, imageUrl } = props.activeUser;
   return (
-    <div className='user-wraper'>
-      <div className='user-profile'>
-        <img alt='the current user profile pic' src={imageUrl}/>
+    <div className="user-wraper">
+      <div className="user-profile">
+        <img alt="the current user profile pic" src={imageUrl} />
         <div>
           <h1>User: {first_name + ' ' + last_name}</h1>
           <h2>Listed Contact: {email}</h2>
