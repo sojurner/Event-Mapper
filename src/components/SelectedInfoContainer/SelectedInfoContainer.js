29f749816a4804302037ee6bfa80221c0f47fe7e
sojurner <pykim0591@gmail.com>
2018-10-31 08:07:25 -0600

    Check proptypes

--- src/components/SelectedInfoContainer/SelectedInfoContainer.js
+++ src/components/SelectedInfoContainer/SelectedInfoContainer.js
@@ -6,7 +6,7 @@ export const SelectedInfoContainer = ({ removeEvent, item, weather }) => {
   return weather ? (
     <div className="selected-container">
       <section className="weather-info">
-      <img src={require(`../../images/${weather.icon}.png`)} />
+        <img src={require(`../../images/${weather.icon}.png`)} />
         <p className="weather-high">
           {Math.floor(weather.high)}
           °F
@@ -29,9 +29,17 @@ export const SelectedInfoContainer = ({ removeEvent, item, weather }) => {
         Remove from Watchlist
       </button>
     </div>
+  ) : (
+    <img
+      src={`url(https://steamusercontent-a.akamaihd.net/ugc/82592888811466982/5C5EF117E40A35C384624D15C32BEF0B1F5D8D3C/)`}
+    />
   );
 };
 
 SelectedInfoContainer.propTypes = {
-  displayInfo: PropTypes.object
+  removeEvent: PropTypes.func,
+  item: PropTypes.object,
+  weather: PropTypes.object
 };
+
+// {"summary":"Partly cloudy throughout the day.","icon":"partly-cloudy-day","precip":0.1,"high":46.67,"low":26.7}
