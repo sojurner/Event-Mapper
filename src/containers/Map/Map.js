193ed060ff97710216142cf4b4c63faf4aca262d
sojurner <pykim0591@gmail.com>
2018-11-19 17:42:49 -0700

    Import Feature and Layer Components

--- src/containers/Map/Map.js
+++ src/containers/Map/Map.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import ReactMapboxGl from 'react-mapbox-gl';
+import ReactMapboxGl, { Feature, Layer } from 'react-mapbox-gl';
 
 import { UserLocation } from '../../components/UserLocation/UserLocation';
-import Events from '../Events/Events';
+import { EventPopup } from '../../components/EventPopup/EventPopup';
 import { setEvents } from '../../actions/index.js';
 import { getEvents } from '../../utilities/apiCalls/apiCalls';
 
