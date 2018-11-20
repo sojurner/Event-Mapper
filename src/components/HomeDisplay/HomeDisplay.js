936172e753ed52805c45cdfd726586bc6ef16093
sojurner <pykim0591@gmail.com>
2018-11-19 17:39:31 -0700

    Migrate Event render to HomeDisplay

--- src/components/HomeDisplay/HomeDisplay.js
+++ src/components/HomeDisplay/HomeDisplay.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Map from '../../containers/Map/Map';
+import Events from '../../containers/Events/Events';
 import FilterBar from '../../containers/FilterBar/FilterBar';
 
 export const HomeDisplay = () => (
   <div>
     <div>
       <FilterBar />
+      <Events />
       <Map />
     </div>
   </div>
