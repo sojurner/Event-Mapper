ae6ee213628aaab1be57b6ae23fa51825fb78944
sojurner <pykim0591@gmail.com>
2018-11-29 07:21:00 -0700

    Production build

---
+++ build/precache-manifest.141f59cc1a798ef7c6fa45e1b9d3376d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ede01e26860047da63c2dc19ab1b196",
+    "url": "/static/media/concert_crowd.3ede01e2.jpg"
+  },
+  {
+    "revision": "fd8ad8acd98af431846c",
+    "url": "/static/css/main.896e6f10.chunk.css"
+  },
+  {
+    "revision": "6d810c906a88cb9f43a3dad98d7bb035",
+    "url": "/static/media/xtreem.6d810c90.ttf"
+  },
+  {
+    "revision": "9c476249225f4d806dd6",
+    "url": "/static/js/1.9c476249.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4164f0b207e12ab36990cd2a7e7b0acb",
+    "url": "/static/media/location-point.4164f0b2.png"
+  },
+  {
+    "revision": "bef9749a5d0fd6999aea4bb8f61e3594",
+    "url": "/static/media/loading.bef9749a.gif"
+  },
+  {
+    "revision": "fd8ad8acd98af431846c",
+    "url": "/static/js/main.fd8ad8ac.chunk.js"
+  },
+  {
+    "revision": "65ab651cc7cebf7331a4228e830573d1",
+    "url": "/static/media/CaviarDreams_Bold.65ab651c.ttf"
+  },
+  {
+    "revision": "3670aa493ee09e92c7b8e1e7c2f5b441",
+    "url": "/static/media/CaviarDreams.3670aa49.ttf"
+  },
+  {
+    "revision": "3d7c025a08fa8637bb533d248115d3e2",
+    "url": "/static/media/earth.3d7c025a.png"
+  },
+  {
+    "revision": "5361d4c5effb5de08460acc1af57e273",
+    "url": "/static/media/map-pin.5361d4c5.png"
+  },
+  {
+    "revision": "a7e8347ac0bf5ff6aa813c0e8b5c23bc",
+    "url": "/static/media/codec.a7e8347a.ttf"
+  },
+  {
+    "revision": "9c476249225f4d806dd6",
+    "url": "/static/css/1.46967712.chunk.css"
+  },
+  {
+    "revision": "aac446e6a28fc3e9ad9243df335a0b93",
+    "url": "/index.html"
+  }
+];
