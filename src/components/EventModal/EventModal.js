8c977d4708cabfca885104135099abeb99d377c7
sojurner <pykim0591@gmail.com>
2018-11-29 18:29:35 -0700

    Reduce title string length

--- src/components/EventModal/EventModal.js
+++ src/components/EventModal/EventModal.js
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 import './EventModal.css';
 
 export const EventModal = ({ targetEvent }) => {
-  const { name, img, date, venue_name, address } = targetEvent;
+  const { img, date, venue_name, address } = targetEvent;
+  let name = targetEvent.name;
+  if (targetEvent.name > 40) {
+    name = `${name.slice(0, 40)}...`;
+  }
   return (
     <div className="event-modal-container">
       <h1 className="event-name">{name}</h1>
-      <h4 className="event-date">{date}</h4>
-      <img
-        src={`${img}`}
-        alt="current event"
-        height={400}
-        className="event-img"
-      />
-      <p className="event-venue">{venue_name}</p>
-      <p className="event-address">{address}</p>
+      <section className="event-modal-contents">
+        <img
+          src={`${img}`}
+          alt="current event"
+          height={390}
+          className="event-img"
+        />
+        <p className="event-venue">{`${venue_name}`}</p>
+        <p className="event-address">{address}</p>
+        <h4 className="event-date">{date}</h4>
+      </section>
     </div>
   );
 };
