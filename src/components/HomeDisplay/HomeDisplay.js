0e55f2139957b48c7c91e5b42f3423514dab226a
sojurner <pykim0591@gmail.com>
2018-11-30 17:49:10 -0700

    Remove inner div element

--- src/components/HomeDisplay/HomeDisplay.js
+++ src/components/HomeDisplay/HomeDisplay.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Map from '../../containers/Map/Map';
 import Events from '../../containers/Events/Events';
 import FilterBar from '../../containers/FilterBar/FilterBar';
+import './HomeDisplay.css';
 
 export const HomeDisplay = () => (
-  <div>
-    <div>
-      <FilterBar />
-      <Events />
-      <Map />
-    </div>
+  <div className="home-container">
+    <FilterBar />
+    <Events />
+    <Map />
   </div>
 );
