071c5a0f972dbe66971df103b162d6da3aac4da2
sojurner <pykim0591@gmail.com>
2018-12-30 14:10:34 -0700

    Create reducer method for set map center

--- src/reducers/mapReducer.js
+++ src/reducers/mapReducer.js
@@ -7,6 +7,15 @@ export const mapZoomReducer = (state = [12], action) => {
   }
 };
 
+export const mapCenterReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_MAP_CENTER':
+      return [action.coords.longitude, action.coords.latitude];
+    default:
+      return state;
+  }
+};
+
 export const mapDisplayReducer = (state = false, action) => {
   switch (action.type) {
     case 'SET_MAP_DISPLAY':
