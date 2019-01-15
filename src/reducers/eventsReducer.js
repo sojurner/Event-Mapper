e5fe4201e37f70f9e1e346acc92478356b9e3e88
sojurner <pykim0591@gmail.com>
2019-01-15 16:10:09 -0700

    Refactor event reducers to account for current page

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -1,18 +1,34 @@
-export const eventsReducer = (state = [], action) => {
+export const eventsReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_EVENTS':
-      return action.events || state;
+      state[action.page] = action.events;
+      return state;
 
     case 'SET_WATCH_EVENT':
-      const events = state.map(event => {
-        if (event.e_id === action.event.e_id) {
-          event.favorite = !event.favorite;
-        }
-        return event;
-      });
+      state[action.page][state[action.page].indexOf(action.event)] =
+        action.event;
+      return state;
 
-      return events;
+    default:
+      return state;
+  }
+};
 
+export const eventsPageReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'SET_EVENT_PAGE_INFO':
+      return action.info;
+    case 'SET_CURRENT_EVENT_PAGE':
+      return { ...state, current: action.page };
+    default:
+      return state;
+  }
+};
+
+export const eventsLinkReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'SET_EVENT_LINK_INFO':
+      return action.links;
     default:
       return state;
   }
