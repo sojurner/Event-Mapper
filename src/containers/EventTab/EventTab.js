e17d436dbeeb7af3c83e364c0382415d2bf03145
sojurner <pykim0591@gmail.com>
2019-01-15 11:20:43 -0700

    transfer favorite methods to event tab

--- src/containers/EventTab/EventTab.js
+++ src/containers/EventTab/EventTab.js
@@ -1,17 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as clean from '../../utilities/helpers/helpers';
+import * as call from '../../utilities/apiCalls/apiCalls';
+import * as invoke from '../../actions';
+
 import './EventTab.css';
 
 export class EventTab extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayTab: true,
+      msgPrompt: ''
+    };
+  }
+
+  toggleTab = () => {
+    this.setState(prevState => ({
+      displayTab: !prevState.displayTab
+    }));
+  };
+
+  handleFavoriteClick = async (eve, event) => {
+    eve.stopPropagation();
+    const {
+      activeUser,
+      addToWatchList,
+      setWatchEvent,
+      watchList,
+      removeFromWatchlist
+    } = this.props;
+    if (!event.favorite) {
+      event.favorite = true;
+      setWatchEvent(event);
+      const body = clean.eventServerCleaner(activeUser, event);
+      const response = await call.setFavorite(
+        body.userObj,
+        body.eventObj,
+        activeUser.id
+      );
+      if (!response.error) {
+        addToWatchList(response.event);
+        this.setState({ msgPrompt: 'Event Saved!' });
+      }
+    } else {
+      event.favorite = false;
+      const matchingEvent = watchList.find(item => item.e_id === event.e_id);
+      await call.removeFromWatchlist(activeUser.id, matchingEvent.id);
+      setWatchEvent(event);
+      removeFromWatchlist(matchingEvent);
+      this.setState({ msgPrompt: 'Event Removed!' });
+    }
+
+    setTimeout(() => {
+      this.setState({ msgPrompt: '' });
+    }, 2000);
+  };
+
   render() {
     const {
       events,
       showEventInfo,
       handleModalClick,
-      handleFavoriteClick,
       closePopup,
-      displayTab
+      userLocation
     } = this.props;
+
+    const { msgPrompt, displayTab } = this.state;
+
     const eventTab = events.map((event, index) => {
       return (
         <div
@@ -29,7 +85,7 @@ export class EventTab extends Component {
                   ? 'fas fa-bookmark'
                   : 'fas fa-bookmark active-bookmark'
               }
-              onClick={eve => handleFavoriteClick(eve, event)}
+              onClick={eve => this.handleFavoriteClick(eve, event)}
             />
             <h1 className="tab-contents tab-event-name">{event.name}</h1>
             <p className="tab-contents tab-date">{event.date}</p>
@@ -47,13 +103,23 @@ export class EventTab extends Component {
       <div
         className={displayTab ? 'tab-container' : 'tab-container tab-inactive'}
       >
+        <div onClick={this.toggleTab} className="chevron-left-right-container">
+          <i
+            className={
+              displayTab
+                ? 'fas fa-chevron-circle-left'
+                : 'fas fa-chevron-circle-right'
+            }
+          />
+        </div>
+        {msgPrompt && <div className="prompt-msg">{msgPrompt}</div>}
         <h2 className="event-header-title">
-          {this.props.userLocation.hasOwnProperty('location')
-            ? this.props.userLocation.location
+          {userLocation.hasOwnProperty('location')
+            ? userLocation.location
             : 'Events Near You'}{' '}
           <strong>({events.length})</strong>
         </h2>
-        {displayTab && <div className="tab-scroll-container">{eventTab}</div>}
+        <div className="tab-scroll-container">{eventTab}</div>
       </div>
     );
   }
@@ -61,8 +127,18 @@ export class EventTab extends Component {
 
 export const mapStateToProps = state => ({
   userLocation: state.userLocation,
+  activeUser: state.activeUser,
   events: state.events,
   watchList: state.watchList
 });
 
-export default connect(mapStateToProps)(EventTab);
+export const mapDispatchToProps = dispatch => ({
+  removeFromWatchlist: event => dispatch(invoke.removeFromWatchlist(event)),
+  addToWatchList: event => dispatch(invoke.addToWatchList(event)),
+  setWatchEvent: event => dispatch(invoke.setWatchEvent(event))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EventTab);
